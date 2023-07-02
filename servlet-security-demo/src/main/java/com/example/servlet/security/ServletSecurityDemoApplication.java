64566c45acc910ecb5b4c6c2e3538a1076546086
jinchao <jinchao@vandream.com>
2023-07-02 22:24:34 +0800

    spring security

---
+++ servlet-security-demo/src/main/java/com/example/servlet/security/ServletSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.servlet.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletSecurityDemoApplication.class, args);
+	}
+
+}
